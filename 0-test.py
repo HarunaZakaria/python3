d6587c3b9a34626b3dab3c38ca9a8306ce3350bf
harunazakaria <Harunzy55@gmail.com>
2022-11-05 18:38:19 +0000

    a new commit

---
+++ 0-test.py
@@ -0,0 +1,8 @@
+#buliant values
+haruna = False
+zakaria = True
+if haruna:
+    print("you are welcome to the family Mr. Haruna")
+elif zakaria:
+    print("Good morning Mr. Zakaria")
+print("thank you!!")
---
+++ 1-test.py
@@ -0,0 +1,9 @@
+#logical  and operators
+has_high_income = True
+has_good_credit = False
+
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+#logical or operator
+if has_high_income or has_good_credit:
+    print("Eligible for loan")
